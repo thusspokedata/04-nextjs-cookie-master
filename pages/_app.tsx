efe6b7853d8ad4c6dafd27deedb919f440450074
antoniosaleme <kilo@disroot.org>
2023-07-01 14:22:20 +0200

    feat: starting

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
+import { darkTheme, lightTheme, customTheme } from '../themes'
+import { CssBaseline } from '@mui/material'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return 
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+      <Component {...pageProps} />
+  </ThemeProvider>
+}
